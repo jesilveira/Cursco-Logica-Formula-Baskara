a58a4cf2d91090d96f95d2bccbd4ef246606c1e9
Jessika <jessika.santos.silveira@gmail.com>
2022-06-13 15:45:40 -0300

    Subindo projeto

---
+++ src/operacao/Main.java
@@ -0,0 +1,33 @@
+package operacao;
+import java.util.Scanner;
+import java.util.Locale;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		double c = sc.nextDouble();
+		
+		double delta = b * b - 4 * a * c;
+		
+		if (a==0 || delta < 0.0) {
+			System.out.printf("Impossível calcular");
+		}
+		
+		else {
+			double r1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+			double r2 = (-b - Math.sqrt(delta)) / (2.0 * a);
+			System.out.printf("R1 = %.5f%n" , r1);
+			System.out.printf("R2 = %.5f%n" , r2);
+		}			
+		
+		sc.close();
+	}
+
+}
